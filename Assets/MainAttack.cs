0dbfdd9be6f349057f4951fd436a395fa925017b
Anders Meinicke <andersmeinicke@gmail.com>
2023-04-19 12:47:01 +0200

    Player+EarlyAttack

---
+++ Assets/MainAttack.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainAttack : MonoBehaviour
+{
+    private GameObject playerObj = null;
+    Vector2 location;
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerObj = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            Debug.Log("Input.mousePosition");
+            location = playerObj.transform.position;
+            GameObject go1;
+            go1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            go1.transform.position = location;
+        }
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f; // movement speed in units per second
+
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // read input axes
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // apply movement to rigidbody
+        rb2d.MovePosition(rb2d.position + movement.normalized * speed * Time.fixedDeltaTime);
+    }
+}
