b309091ffaf2175c812093f009a0181263613857
rasm445f <90387951+rasm445f@users.noreply.github.com>
2023-04-26 13:25:41 +0200

    rasmus first commit

---
+++ Assets/Health.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+    [SerializeField] private Animator animator;
+    private int currentHealth;
+
+    public UnityEvent OnDeath;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        if (animator != null)
+        {
+            animator.SetBool("die", true);
+            OnDeath.Invoke();
+        }
+    }
+}
