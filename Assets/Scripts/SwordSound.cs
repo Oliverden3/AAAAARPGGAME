5d63adac5b6c7bad31a588110cf8a3c1f0a5a42a
Oliverden3 <oliversjensen@hotmail.com>
2023-05-29 12:32:56 +0200

    Added soundfx

---
+++ Assets/Scripts/SwordSound.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSound : MonoBehaviour
+{
+    public AudioClip soundEffect;
+    bool isAttacking;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAttacking = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetMouseButtonDown(0) && isAttacking == false)
+    {
+        PlaySound();
+    }
+    }
+    private void PlaySound(){
+        AudioSource.PlayClipAtPoint(soundEffect, transform.position);
+        isAttacking = true;
+        StartCoroutine(WaitASec());
+
+    }
+
+    IEnumerator WaitASec(){
+    yield return new WaitForSeconds(0.5f);
+    
+    StartCoroutine(AttackCooldown());
+    }
+    IEnumerator AttackCooldown(){
+      yield return new WaitForSeconds(2.0f);
+      isAttacking = false;
+    }
+    
+}
